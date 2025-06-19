85d54b2c48c43a00f18628c2b138900bd4e762b7
Andrii Dmtryk <admitruk237@gmail.com>
2025-06-19 15:26:17 +0200

    refactoting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,14 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gray-50`}
       >
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+        />
         <Header />
-
         <main className="@container max-w-7xl py-7 mx-auto px-4 sm:px-6 lg:px-8">
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-          />
           {children}
         </main>
       </body>
